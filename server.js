76483d2f03ce541270092b780dc0dc9826535f11
sridharjaya <sridharraja2013@gmail.com>
2016-09-26 22:30:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article one',function (req,res){
+    res.send('article one is here');
+});
+
+app.get('/article 2',function (req,res){
+    res.send('article 2 is here');
+});
+
+app.get('/article 3',function (req,res){
+    res.send('article 3 is here');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
